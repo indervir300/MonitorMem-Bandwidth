fa5f945bba438142e326f3bc4a3b519b0466c141
indervir300 <140889625+indervir300@users.noreply.github.com>
2024-07-04 17:50:13 +0530

    Update memUsage.py

--- memUsage.py
+++ memUsage.py
@@ -5,7 +5,7 @@ import psutil
 def update_ram_label():
     ram_percent = psutil.virtual_memory().percent
     ram_label.config(text=f"Mem: {ram_percent:.2f}%")
-    root.after(400, update_ram_label) 
+    root.after(600, update_ram_label) 
 
 
 root = tk.Tk()
@@ -19,7 +19,7 @@ screen_height = root.winfo_screenheight()
 
 # Set window size and position
 window_width = 105
-window_height = 25
+window_height = 32
 taskbar_height = 50  # Adjust this based on your taskbar height
 window_x = screen_width - window_width
 window_y = screen_height - window_height - taskbar_height
@@ -29,8 +29,8 @@ root.geometry(f"{window_width}x{window_height}+{window_x - 4}+{window_y}")
 # Make the RamUsage window stay on top
 root.wm_attributes("-topmost", 1)
 
-ram_label = tk.Label(root, font=("Arial", 10), bg="#212121", fg="#f5f5f5")
-ram_label.pack(padx=5, pady=2)
+ram_label = tk.Label(root, font=("Arial", 10, "bold"), bg="#212121", fg="#f5f5f5")
+ram_label.pack(padx=10, pady=5)
 
 update_ram_label()  # Calling the Function
 
